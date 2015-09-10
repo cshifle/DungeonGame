c92bc12dfbf25fb3e74ff59608b84c56104fa4cb
cshifle <cshiffle1991@gmail.com>
2015-09-10 13:05:35 -0400

    Commit 1

--- src/dungeongame/Room.java
+++ src/dungeongame/Room.java
@@ -5,11 +5,57 @@
  */
 
 package dungeongame;
+import java.util.ArrayList;
 
-/**
- *
- * @author Christopher
- */
+
 public class Room {
+    //create instance variables
+    String title, desc;
+    boolean beenHere;
+    ArrayList exit = new ArrayList<Exit>();
     
+    public Room(String title, String desc, boolean beenHere){
+        
+        //set values of instance variables to those in the constructors
+        this.title = title;
+        this.desc = desc;
+        this.beenHere = beenHere;
+        
+    }
+    
+    public String getTitle(){
+        return title;
+    }
+    public void setDesc(String d){
+        desc = d;
+    }
+    public String describe(){
+        
+        /*The describe() method returns a string of text that is of appropriate 
+        length (i.e., it only includes the description if the user is new to the
+        room.) A Room also maintains a list of Exits, each of which is associated
+        with a direction. A description of each exit will be included in the
+        describe() output, as explained above.  */
+        
+       if(beenHere){
+           return desc;
+      
+        }else{
+           return desc; //just placeholder
+       }
+    }
+    
+    public Room leaveBy(String dir){
+        
+        /*The leaveBy() method will return the Room object reachable by the exit
+        in that direction, if any. (Just return null if there is no exit in that
+        direction.) */
+        
+        return null; //placeholder
+    }
+    
+    public void addExit(Exit ex){
+        exit.add(ex);
+        
+    }
 }
